1ec728f1b337e5261e633e78dfdca38ae773cca5
Sebastian Deppisch <git@sebastiandeppisch.de>
2022-05-14 17:57:15 +0200

    add order form

--- resources/js/views/NewOrder.vue
+++ resources/js/views/NewOrder.vue
@@ -100,6 +100,59 @@
 
     </DxForm>
 
+
+  <DxDataGrid
+    :data-source="orderItemsDatasource"
+  >
+    <DxEditing
+      :allow-updating="true"
+      :allow-adding="false"
+      :allow-deleting="false"
+      mode="cell"
+    />
+    <DxColumn
+      caption="Produkt"
+      data-field="product.name"
+      :allow-sorting="false"
+      :allow-editing="false"
+    />
+    <DxColumn
+      caption="Preis"
+      data-field="product.price"
+      :customize-text="formatPrice"
+      :allow-sorting="false"
+      :allow-editing="false"
+
+    />
+    <DxColumn
+      caption="Anzahl"
+      data-field="quantity"
+      :allow-sorting="false"
+      data-type="number"
+      :editor-options="{defaultValue: 0, min: 0, showSpinButtons: true, showClearButton: true}"
+    />
+    <DxSummary
+      :recalculate-while-editing="true"
+      :calculate-custom-summary="calculateSummary"
+      >
+        <DxTotalItem
+          name="priceSum"
+          summary-type="custom"
+          display-format="Gesamtpreis: {0}"
+          show-in-column="quantity"
+        />
+    </DxSummary>
+  </DxDataGrid>
+
+<br><br>
+
+  <DxButton 
+    text="Bestellung absenden"
+    type="default"
+    width="100%"
+    @click="saveOrder"
+  />
+
   <div style="display:none;">
     <!-- otherwise vue will not auto import DxAutocomplete-->
     <DxAutocomplete />
@@ -109,6 +162,15 @@
 </template>
 
 <script setup lang="ts">
+
+import DxButton from 'devextreme-vue/button';
+
+import DxDataGrid, {
+  DxColumn,
+  DxEditing,
+  DxSummary,
+  DxTotalItem
+} from "devextreme-vue/data-grid";
 
 import { DxAutocomplete } from 'devextreme-vue/autocomplete';
 
@@ -120,12 +182,33 @@ import axios, {AxiosError} from 'axios';
 import { ValidationCallbackData } from 'devextreme/ui/validation_rules';
 import {ref} from 'vue';
 
+import DataSource from "devextreme/data/data_source";
+import CustomStore from 'devextreme/data/custom_store';
+import { resolveSoa } from "dns";
+import { CustomSummaryInfo } from "devextreme/ui/data_grid";
+
 let formData: any = ref({});
 
 let citySuggestions = ref([]);
 
+
+function saveOrder(){
+  console.log("saveOrder");
+}
+
+
+function formatPrice(price){
+  return parseFloat(price.value).toFixed(2).replace(".", ",") + " €";
+}
+
+function calculateSummary(options: CustomSummaryInfo){
+  options.totalValue = orderItems.reduce(
+    (sum, orderItem) => sum + orderItem.quantity*orderItem.product.price,
+    0).toFixed(2).replace(".", ",") + " €";
+}
+
 function validateAsync(params: ValidationCallbackData){
-  const data = {...formData};
+  const data = {...formData.value};
   const field: string = params.formItem.dataField;
   data[field] = params.value;
   console.log(data);
@@ -157,7 +240,6 @@ const subUrbZips = {
 function zipChanged(e){
   let suggestion = [];
   let zip = parseInt(formData.value.zip);
-  console.log(formData.value.zip);
 
   if(cityZips.includes(zip)){
     suggestion.push('Darmstadt');
@@ -168,7 +250,75 @@ function zipChanged(e){
   if(suggestion.length === 1){
     formData.value.city = suggestion[0];
   }
-  citySuggestions.value = suggestion;
+  citySuggestions.value.length = 0;
+  suggestion.forEach((s) => {
+    citySuggestions.value.push(s);
+  })
 }
+
+interface Product{
+  name: string;
+  price: number;
+  sku: string;
+  panels: number;
+  url: string;
+  description: string;
+  id: number;
+}
+
+interface OrderItem{
+  product: Product, 
+  quantity: number,
+  id: number
+}
+
+
+let orderItems: Array<OrderItem> = [];
+
+let orderItemsDatasource = new DataSource({
+  store: new CustomStore({
+    key: 'id',
+    load: (options) => {
+      if(orderItems.length === 0){
+        return axios.get('api/products',{
+          params: options
+        }).then(response =>  {
+          return orderItems = (response.data as Array<Product>).map(product => {
+            return {
+              product: product, 
+              quantity: 0,
+              id: product.id
+            }
+          }) as Array<OrderItem>
+        });
+      }
+      return new Promise((resolve, reject) => {
+        resolve(orderItems);
+      });
+    },
+    update: (key, values: OrderItem) => {
+      return new Promise((resolve, reject) => {
+        let item: OrderItem|undefined = orderItems.find((orderItem => orderItem.id === key));
+        if(item === undefined){
+          reject("item not found");
+        }
+        if(values.quantity === null){
+          values.quantity = 0;
+        }
+        item.quantity = values.quantity;
+        return resolve(item);
+      })
+    },
+    byKey: (key) => {
+      return new Promise((resolve, reject) => {
+        let item: OrderItem|undefined = orderItems.find((orderItem => orderItem.id === key));
+        if(item === undefined){
+          reject("item not found");
+        }
+        return resolve(item);
+      })
+    }
+  })
+})
 
 </script>
