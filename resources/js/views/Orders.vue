5f19b8fd1d58479459e0c5939c9795f46f664c01
Sebastian Deppisch <git@sebastiandeppisch.de>
2022-05-15 01:11:58 +0200

    add summary and store

--- resources/js/views/Orders.vue
+++ resources/js/views/Orders.vue
@@ -4,27 +4,50 @@
 
     <DxDataGrid
       class="dx-card wide-card"
-      data-source="api/orders"
+      :data-source="ordersStore"
       :show-borders="false"
       :column-auto-width="true"
       :column-hiding-enabled="true"
-      key-expr="id"
     >
       <DxColumn data-field="firstName" caption="Vorname" />
       <DxColumn data-field="lastName" caption="Nachname" />
-      <DxColumn data-field="panelCount" caption="Anzahl Module" />
-      <DxColumn data-field="price" caption="Gesamtpreis" />
+      <DxColumn data-field="panelsCount" caption="Anzahl Module" />
+      <DxColumn data-field="price" caption="Gesamtpreis" :customize-text="formatPriceCell" />
+
+       <DxSummary
+        :recalculate-while-editing="true"
+        >
+          <DxTotalItem
+            column="price"
+            summary-type="sum"
+            value-format="currency"
+            display-format="Gesamtpreis: {0}"
+          />
+          <DxTotalItem
+            column="panelsCount"
+            summary-type="sum"
+            display-format="Panels gesamt: {0}"
+          />
+      </DxSummary>
     </DxDataGrid>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+
+import LaravelDataSource from '../LaravelDataSource'
+import {formatPriceCell, formatPrice} from '../helpers'
 import { ref, onMounted } from 'vue'
-import "devextreme/data/odata/store";
+import { CustomSummaryInfo } from "devextreme/ui/data_grid";
 import DxDataGrid, {
   DxColumn,
-  DxEditing
+  DxEditing, 
+  DxSummary,
+  DxTotalItem
 } from "devextreme-vue/data-grid";
 
-const testData = [];
+type Order = App.Models.Order;
+
+const ordersStore = new LaravelDataSource("api/orders");
+
 </script>
