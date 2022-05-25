7c757c0124dfd2d6d46d62c4f9f828bd9b781478
Sebastian Deppisch <git@sebastiandeppisch.de>
2022-05-25 16:46:13 +0200

    add user API

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+
+class UserController extends Controller
+{
+
+    private function dxFilter(Request $request, Builder $builder): Builder{
+        if(isset($request->searchOperation) && isset($request->searchValue) && isset($request->searchExpr)){
+			if($request->searchOperation === "contains"){
+				$builder = $builder->where($request->searchExpr, 'like', "%".$request->searchValue."%");
+			}
+		}
+        return $builder;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return $this->dxFilter($request, User::query())->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $user = new User($request->all());
+        $user->save();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUserRequest  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->fill($request->all());
+        $user->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+    }
+
+    public function show(User $user){
+        return $user;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderItemController;
@@ -22,6 +23,7 @@ require_once(__DIR__."/api.auth.php");
 Route::middleware('auth')->group(function () {
     Route::resource('orders', OrderController::class);
     Route::resource('products', ProductController::class);
+    Route::resource('users', UserController::class);
     
     Route::scopeBindings()->group(function(){
         Route::resource('orders.orderitems', OrderItemController::class);
