68f8ea617e2233c289f94d6434fd8a5b00d986c9
Sebastian Deppisch <git@sebastiandeppisch.de>
2022-05-26 16:07:43 +0200

    fix controller

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\OrderItem;
-use App\Http\Requests\StoreProductRequest;
-use App\Http\Requests\UpdateProductRequest;
 use App\Http\Requests\StoreOrderItemRequest;
+use App\Http\Requests\UpdateOrderItemRequest;
 
 class OrderItemController extends Controller
 {
@@ -21,17 +20,23 @@ class OrderItemController extends Controller
         $orderItem->order_id = $order->id;
         $orderItem->save();
         $order->normalize();
+        return $orderItem;
     }
 
-    public function update(Order $order, OrderItem $orderitem, UpdateProductRequest $request)
+    public function update(Order $order, OrderItem $orderitem, UpdateOrderItemRequest $request)
     {
         $orderitem->fill($request->all());
         $orderitem->save();
         $order->normalize();
+        return $orderitem;
     }
 
     public function destroy(Order $order, OrderItem $orderitem)
     {
         $orderitem->delete();
+    }
+
+    public function show(Order $order, OrderItem $orderitem){
+        return $orderitem;
     }
 }
