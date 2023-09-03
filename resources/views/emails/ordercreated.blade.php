37390c5ee4a9842a6d8ff9f33b2d27c3c1d8e54a
Sebastian Deppisch <git@sebastiandeppisch.de>
2023-09-03 20:12:27 +0200

    change wording

--- resources/views/emails/ordercreated.blade.php
+++ resources/views/emails/ordercreated.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
 # Hallo {{$order->name}}
-Wir haben Deine Bestellung erhalten und melden uns bei Dir, sobald wir sie an unseren Lieferanten weiter geleitet haben. 
+Wir haben Deinen Bestellwunsch erhalten und melden uns bei Dir, sobald wir sie an unseren Lieferanten weiter geleitet haben. 
 Bei Fragen kannst Du auf diese E-Mail antworten, oder Dich an Deine\*n Berater\*in {{$order->advisor->name}} wenden.
 
-Zur Kontrolle ist Deine Bestellung nochmal aufgeführt:
+Zur Kontrolle ist Dein Bestellwunsch nochmal aufgeführt:
 
 @include('emails.order')
 
