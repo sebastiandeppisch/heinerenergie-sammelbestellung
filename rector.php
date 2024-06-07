6161b9c5673f619aab58ca3b0f3c8457ba7ae49e
Sebastian Deppisch <git@sebastiandeppisch.de>
2024-06-07 16:26:35 +0200

    add laravel & php 8.1 config

--- rector.php
+++ rector.php
@@ -3,19 +3,21 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
         __DIR__.'/config',
+        __DIR__.'/database',
         __DIR__.'/node_modules',
         __DIR__.'/public',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
+    ->withPhpSets(php81: true)
     ->withRules([
+    ])->withSets([
+        LaravelSetList::LARAVEL_90,
     ]);
