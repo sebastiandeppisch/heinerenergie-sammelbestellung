4aca55bd450ea0f40a00c7fd006027964c4f8fcf
Sebastian Deppisch <git@sebastiandeppisch.de>
2024-08-23 20:04:01 +0200

    remove appinfo

--- resources/js/helper.ts
+++ resources/js/helper.ts
@@ -12,8 +12,6 @@ import './themes/generated/theme.base.css';
 import './themes/generated/theme.additional.css';
 import { createApp }  from "vue";
 
-import appInfo from "./app-info";
-
 
 import library from './fontawesome'
 library.add()
@@ -24,5 +22,4 @@ import NewAdvice from './views/NewAdvice.vue'
 const newAdvice = createApp(NewAdvice);
 newAdvice.component('font-awesome-icon', FontAwesomeIcon)
 
-newAdvice.config.globalProperties.$appInfo = appInfo;
 newAdvice.mount('#new-advice');
