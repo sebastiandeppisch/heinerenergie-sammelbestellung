e8dd76a55569a8232f5c792fe776070c36143b20
Sebastian Deppisch <git@sebastiandeppisch.de>
2024-08-23 20:20:41 +0200

    add inertia config

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,12 +2,12 @@ import './bootstrap';
 //import 'devextreme/dist/css/dx.common.css';
 import './themes/generated/theme.base.css';
 import './themes/generated/theme.additional.css';
-import { createApp }  from "vue";
-import router from "./router";
+import { createApp, h, DefineComponent } from 'vue'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import App from "./App.vue";
-import appInfo from "./app-info";
 import { store, key } from './store';
+import { createInertiaApp } from '@inertiajs/vue3'
+
 
 import library from './fontawesome'
 library.add()
@@ -15,11 +15,21 @@ library.add()
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import auth from './auth';
 
-const app = createApp(App);
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(store, key);
-app.use(router);
-app.config.globalProperties.$appInfo = appInfo;
-app.mount('#app');
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-auth.initLogin();
+createInertiaApp({
+	title: (title) => `${title} - ${appName}`,
+	resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+	setup({ el, App, props, plugin }) {
+		const app = createApp({ render: () => h(App, props) })
+		app.use(plugin)
+		app.mount(el)
+		//TODO add Ziggy
+
+		app.component('font-awesome-icon', FontAwesomeIcon)
+		app.use(store, key);
+		//app.use(router);
+
+		auth.initLogin(); //TODO refactor auth management
+	},
+})
