dda43687be2dd933b90908ebc674669932961094
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-02-15 17:36:18 +0100

    install tailwind

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,5 @@
 import './bootstrap';
+import '../css/app.css';
 //import 'devextreme/dist/css/dx.common.css';
 import './themes/generated/theme.base.css';
 import './themes/generated/theme.additional.css';
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         laravel([
+            'resources/css/app.css',
             'resources/js/app.ts',
-			'resources/js/helper.ts'
+            'resources/js/helper.ts'
         ]),
         vue({
          template: {
@@ -16,6 +18,7 @@ export default defineConfig({
                  },
              },
         }),
+        tailwindcss()
     ],
 	resolve: {
         alias: {
