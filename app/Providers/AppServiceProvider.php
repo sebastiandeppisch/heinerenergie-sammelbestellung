04facba9a43faee7dbd48f53af46efee963c805b
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-02-19 19:33:40 +0100

    mock coordinate fetching for tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use maxh\Nominatim\Nominatim;
+use Illuminate\Support\ServiceProvider;
 use Opcodes\LogViewer\Facades\LogViewer;
+use App\Actions\FetchCoordinateByAddress;
+use App\ValueObjects\Address;
+use App\ValueObjects\Coordinate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,12 +32,25 @@ class AppServiceProvider extends ServiceProvider
             return $request->user()?->email === config('app.admin_email');
         });
 
-        $this->app->bind(Nominatim::class, function () {
-            $url = "http://nominatim.openstreetmap.org/";
-            $defaultHeader = [
-                'User-Agent' => 'heiner*energie CMS'
-            ];
-            return new Nominatim($url, $defaultHeader);
-        });
+        if(config('app.env') === 'testing') {
+            $this->app->bind(FetchCoordinateByAddress::class, function () {
+
+                $coordinatesOfDarmstadtCenter = new Coordinate(
+                    lat: 49.8728475,
+                    long: 8.6510204
+                );
+
+                return fn (Address $address) => $coordinatesOfDarmstadtCenter;
+            });
+        }else{
+            $this->app->bind(Nominatim::class, function () {
+                $url = "http://nominatim.openstreetmap.org/";
+                $defaultHeader = [
+                    'User-Agent' => 'heiner*energie CMS'
+                ];
+                return new Nominatim($url, $defaultHeader);
+            });
+        }
+
     }
 }
