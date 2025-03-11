314f8cb73a1fa7c9f97020d66f83efaf32d712d4
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-03-11 11:31:25 +0100

    make logo optional

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -25,22 +25,9 @@ class GroupFactory extends Factory
      */
     public function definition(): array
     {
-        $isQuadratic = fake()->boolean(50);
-
-        if ($isQuadratic) {
-            $logoPath = 'https://picsum.photos/200';
-        } else {
-            $logoPath = 'https://picsum.photos/200/300';
-        }
-
-        $randomFilename = fake()->word().'.png';
-
-        Storage::disk('public')->put('group-logos/'.$randomFilename, file_get_contents($logoPath));
-
         return [
             'name' => fake()->company(),
             'description' => fake()->optional(0.7)->paragraph(),
-            'logo_path' => 'group-logos/'.$randomFilename,
             'parent_id' => null, // By default create main groups, use state method for child groups
             'accepts_transfers' => fake()->boolean(80), // 80% chance of accepting transfers
         ];
@@ -53,6 +40,25 @@ class GroupFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'parent_id' => Group::factory(),
+        ]);
+    }
+
+    public function withLogo(): static
+    {
+        $isQuadratic = fake()->boolean(50);
+
+        if ($isQuadratic) {
+            $logoPath = 'https://picsum.photos/200';
+        } else {
+            $logoPath = 'https://picsum.photos/200/300';
+        }
+
+        $randomFilename = fake()->word().'.png';
+
+        Storage::disk('public')->put('group-logos/'.$randomFilename, file_get_contents($logoPath));
+
+        return $this->state(fn (array $attributes) => [
+            'logo_path' => 'group-logos/'.$randomFilename,
         ]);
     }
 }
