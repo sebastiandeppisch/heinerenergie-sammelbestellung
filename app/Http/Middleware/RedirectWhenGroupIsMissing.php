54baf14ebbceaf67d512c40a1cfc9c578d3a8c6d
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-03-25 15:15:50 +0100

    fix styling

--- app/Http/Middleware/RedirectWhenGroupIsMissing.php
+++ app/Http/Middleware/RedirectWhenGroupIsMissing.php
@@ -27,7 +27,7 @@ class RedirectWhenGroupIsMissing
                 'reset-password',
                 'actAsSystemAdmin',
                 'dev.login',
-                'newadvice'
+                'newadvice',
             ];
 
             if ($groups->count() === 1) {
