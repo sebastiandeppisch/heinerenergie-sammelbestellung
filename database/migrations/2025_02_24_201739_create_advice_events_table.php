5f55db01a6a9e35f3d2b8f2cae8fe05652ee17dd
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-06-10 14:20:26 +0200

    add primary key

--- database/migrations/2025_02_24_201739_create_advice_events_table.php
+++ database/migrations/2025_02_24_201739_create_advice_events_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('advice_events', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->foreignIdFor(Advice::class)->constrained();
             $table->foreignIdFor(User::class)->nullable()->constrained();
             $table->longText('event');
