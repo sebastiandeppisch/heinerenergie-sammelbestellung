53cb82a0f97dcca50e73d33d6d5fd202f221e7c9
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-06-26 09:50:57 +0200

    add version to favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('favicon.ico').'?'.time() }}">
     <title>Heinerenergie CRM</title>
     @vite(['resources/js/app.ts'])
     @routes
