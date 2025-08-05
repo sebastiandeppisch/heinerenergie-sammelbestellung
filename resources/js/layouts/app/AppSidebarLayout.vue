444b71ba4637d3e61e9f76be93d549d0385677b9
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-08-05 15:31:50 +0200

    Add flash messages to layout

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -4,6 +4,12 @@ import AppShell from '@/layouts/components/AppShell.vue';
 import AppSidebar from '@/layouts/components/AppSidebar.vue';
 import AppSidebarHeader from '@/layouts/components/AppSidebarHeader.vue';
 import type { BreadcrumbItemType } from '@/layouts/helper';
+import { Toaster } from "@/shadcn/components/ui/sonner";
+import 'vue-sonner/style.css' 
+import { PageProps } from "@inertiajs/core";
+import { watch } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+import { toast } from 'vue-sonner';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -12,6 +18,36 @@ interface Props {
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+
+
+
+interface CustomPageProps extends PageProps {
+  flashMessages: {
+    [key: string]: string;
+  };
+}
+const page = usePage<CustomPageProps>();
+
+
+watch(() => page.props.flashMessages, (newVal) => {
+  for (const key in newVal) {
+    if (newVal[key]) {
+        const message = newVal[key];
+        if(key === 'error') {
+          toast.error(message);
+        } else if (key === 'success') {
+          toast.success(message);
+        } else if (key === 'info') {
+          toast.info(message);
+        } else if (key === 'warning') {
+          toast.warning(message);
+        } else {
+          toast(message);
+        }
+    }
+  }
+});
+
 </script>
 
 <template>
@@ -19,6 +55,7 @@ withDefaults(defineProps<Props>(), {
         <AppSidebar />
         <AppContent variant="sidebar" class="overflow-x-hidden bg-slate-50">
             <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+            <Toaster class="pointer-events-auto" style="z-index: 9999;" :richColors="true" position="top-center" />
             <slot />
         </AppContent>
     </AppShell>
