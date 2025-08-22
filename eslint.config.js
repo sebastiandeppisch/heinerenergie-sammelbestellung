f9ad5adbf3ff1fe00ca28a8ccaffaf18757113f9
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-08-22 14:46:25 +0200

    ignore unused variables

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,7 @@ export default defineConfigWithVueTs(
 		rules: {
 			'vue/multi-word-component-names': 'off',
 			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off'
 		},
 	},
 	prettier,
