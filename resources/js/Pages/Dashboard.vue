f01c8ab1e709cb3e760dbcac3cf802dd8c4804b8
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-08-22 14:32:48 +0200

    use card

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
 import KpiDashboard from '@/components/KpiDashboard.vue';
+import Card from '@/shadcn/components/ui/card/Card.vue';
+import CardContent from '@/shadcn/components/ui/card/CardContent.vue';
+import CardHeader from '@/shadcn/components/ui/card/CardHeader.vue';
 const props = defineProps<{
-  advisorInfo: string
+    advisorInfo: string;
 }>();
-
 </script>
 
 <template>
-  <div ref="outer">
-    <h2 class="content-block">Dashboard</h2>
-    <p class="dx-card content" style="padding:16px;margin:32px;" >
-      <h2>Berater*innen-Infos</h2>
-      <div v-html="props.advisorInfo">
-      </div>
-    </p>
+    <div ref="outer">
+        <h2 class="content-block">Dashboard</h2>
+        <Card style="margin: 2rem">
+            <CardHeader>
+                <h2>Berater*innen-Infos</h2>
+            </CardHeader>
+            <CardContent>
+                <div v-html="props.advisorInfo"></div>
+            </CardContent>
+        </Card>
 
-
-    <KpiDashboard />
-  </div>
+        <KpiDashboard />
+    </div>
 </template>
