04d559a965cf34d4e6fb84f52525ac3932499f7a
Sebastian Deppisch <git@sebastiandeppisch.de>
2025-10-08 00:23:44 +0200

    typo

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use App\Services\SessionService;
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ChangePasswordRequest extends FormRequest
