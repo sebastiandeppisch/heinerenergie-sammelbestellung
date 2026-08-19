f5bf41cf5691166ce8f8ed51386259e7545e6ad3
Sebastian Deppisch <git@sebastiandeppisch.de>
2026-08-19 14:00:35 +0000

    add type declarations via rector

--- app/Http/Controllers/AdviceController.php
+++ app/Http/Controllers/AdviceController.php
@@ -229,7 +229,7 @@ class AdviceController extends Controller
 
         $groups = Group::where('accepts_transfers', true)->get()->filter(fn (Group $group): bool => $group->consulting_area !== null)->map(fn (Group $group): GroupMapData => GroupMapData::fromModel($group))->values();
 
-        $advisors = User::where('is_active', true)->get()->filter(fn (User $advisor) => $user->can('view', $advisor))->map(fn (User $user) => UserData::fromModel($user, false))->values();
+        $advisors = User::where('is_active', true)->get()->filter(fn (User $advisor) => $user->can('view', $advisor))->map(fn (User $user): UserData => UserData::fromModel($user, false))->values();
 
         // Get marker from current group, or use default
         $currentGroup = $currentGroupService->getGroup();
