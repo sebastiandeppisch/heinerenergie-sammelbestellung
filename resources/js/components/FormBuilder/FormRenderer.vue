0aefae146c8a86bcc66a2c1d790153ddd41f0d20
GitHub Action <action@github.com>
2026-09-01 13:00:41 +0000

    Auto-format code with Pint, Prettier, ESLint, and Rector
    - Applied Laravel Pint formatting
    - Applied Prettier formatting
    - Applied ESLint auto-fixes
    - Applied Rector refactoring
    
    [skip ci]

--- resources/js/components/FormBuilder/FormRenderer.vue
+++ resources/js/components/FormBuilder/FormRenderer.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { Button } from '@/shadcn/components/ui/button';
+import { useForm } from '@inertiajs/vue3';
 import { computed } from 'vue';
 import FormFieldRenderer from './FormFieldRenderer.vue';
-import { useForm } from '@inertiajs/vue3';
 
 type FormDefinitionData = App.Data.FormDefinitionData;
 
